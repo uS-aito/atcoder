6d8a23424b71888d619156954edbeed703be42dd
yu.saito <h2so40627@gmail.com>
2020-07-27 19:08:46 +0900

    解いた

--- abc/161/b.py
+++ abc/161/b.py
@@ -1,6 +1,13 @@
 n,m = map(int,input().split())
-a = sorted(list(map(int,input().split())))
+a = sorted(list(map(int,input().split())),reverse=True)
 v = sum(a)
 
-for i in range(m):
-    
+# for i in range(1,m+1):
+#     if a[i-1] < v/(4*i):
+#         print("No")
+#         exit(0)
+
+if a[m-1] < v/(4*m):
+    print("No")
+else:
+    print("Yes")
